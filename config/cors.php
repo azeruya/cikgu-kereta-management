06bef8526fee26df13c5dc90df3000f04fb919c7
azeruya <fathiya.azel@gmail.com>
2026-05-21 03:37:50 +0800

    updated CORS for vercel frontend

--- config/cors.php
+++ config/cors.php
@@ -21,7 +21,7 @@ return [
 
     'allowed_origins' => [
         'http://localhost:5173',
-        'https://your-vercel-app.vercel.app',
+        'https://vulcan-auto.vercel.app',
     ],
 
     'allowed_origins_patterns' => [],
